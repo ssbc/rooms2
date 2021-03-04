f96735d6bbbc89a6da7b0e35b4411860b45edabc
cblgh <cblgh@cblgh.org>
2021-03-04 10:46:53 +0100

    annotate external links, dotted border for internal

--- tools/wrap.js
+++ tools/wrap.js
@@ -13,6 +13,13 @@ td, th {
   padding: 0.5rem;
   border: 1px #aaa solid;
 }
+a[href^="http"]::after {
+  content: "⧉";
+}
+a[href^="#"] {
+  text-decoration: none;
+  border-bottom: 1px blue dotted;
+}
 </style>
 </head>
 <script crossorigin src="https://unpkg.com/mermaid@8.9.1/dist/mermaid.min.js"></script>
